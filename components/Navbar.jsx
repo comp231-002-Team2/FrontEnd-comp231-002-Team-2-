0f8578cc0733f6be3e103be13d33d855f4b67256
Manvibolreach Ouk <vibolreach.m.ouk@gmail.com>
2023-04-06 19:23:43 -0400

    fixed:edited navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,19 +5,20 @@ const Navbar = () => {
     const [nav, setNav] = useState(false)
     const handleNav = () => {
         setNav(!nav)
-
     }
-
-
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4  text-black'>
-        <h1 className= 'w-2/4 text-3xl font-bold text-[black]'>FRIENDLY COMMUNITY</h1>
+      <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'>
+          <div className="w-28 bg-blue-300">
+              <h1 className= "whitespace-pre-wrap w-[340px] font-['Sarabun'] text-[25px] leading-[0.9] font-bold text-left uppercase text-black">FRIENDLY COMMUNITY</h1>             
+          </div>      
         <ul className= 'hidden md:flex'>
-            <li className= 'p-4'>community display</li>
-            <li className= 'p-4'>channel</li>
-            <li className= 'p-4'>contact us</li>
-            <li className= 'p-4'>login</li>
-            <li className= 'p-4'>sign up</li>
+            <li className= "whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Community Display</li>
+            <li className= "whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Channel</li>
+            <li className= "whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Contact Us</li>
+            <li className="whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Login</li>
+            <div className="bg-[#ec7777] h-30">
+                <li className= "whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Sign Up</li>
+            </div>
         </ul>
         <div onClick={handleNav} className= 'block md:hidden'>
             { nav ? <AiOutlineClose size ={20}/> : <AiOutlineMenu size={20} /> }
@@ -31,7 +32,7 @@ const Navbar = () => {
             <li className= 'p-4 border-b border-gray-300'>login</li>
             <li className= 'p-4 border-b border-gray-300'>sign up</li>
             </ul>
-        </div>
+        </div>         
     </div>
   )
 }
