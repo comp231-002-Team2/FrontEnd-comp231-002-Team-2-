131b3f28e2e1552a9c5b814fd46433f34a2d963a
ChungHinNg <cng132@my.centennialcollege.ca>
2023-04-16 19:29:52 -0400

    updated contact page made by Man

--- pages/contact.js
+++ pages/contact.js
@@ -1,52 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
-const ContactForm = () => {
-  const [title, setTitle] = useState('');
-  const [logoimage, setLogoimagel] = useState('');
-  const [city, setCity] = useState('');
-  const [postalCode, setPostalCode] = useState('');
-  const [instagramlink, setInstagramlink] = useState('');
-  const [youtubelink, setYoutubelink] = useState('');
-  const [otherwebsitelink, setOtherwebsitelink] = useState('');
-  const [email, setEmail] = useState('');
-  const [phonenumber, setPhonenumber] = useState('');
-  const [description, setDescription] = useState('');
+const MyComponent = () => {
+  const [nav, setNav] = React.useState(false);
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [message, setMessage] = React.useState('');
 
-  const [message, setMessage] = useState('');
-  const [nav, setNav] = useState(false);
-
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
+  const handleNav = () => {
+    setNav(!nav);
   };
 
-  const handleCityChange = (event) => {
-    setCity(event.target.value);
+  const handleNameChange = (event) => {
+    setName(event.target.value);
   };
-  const handlePostalCodeChange = (event) => {
-    setPostalCode(event.target.value);
-  };
-  const handleLogoimagelChange = (event) => {
-    setLogoimagel(event.target.value);
-  };
-  const handleInstagramlinkChange = (event) => {
-    setInstagramlink(event.target.value);
-  };
-  const handleYoutubelinkChange = (event) => {
-    setYoutubelink(event.target.value);
-  };
-  const handleOtherwebsitelinkChange = (event) => {
-    setOtherwebsitelink(event.target.value);
-  };
+
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
-  };
-  const handlePhonenumberChange = (event) => {
-    setPhonenumber(event.target.value);
-  };
-  const handleDescriptionChange = (event) => {
-    setDescription(event.target.value);
   };
 
   const handleMessageChange = (event) => {
@@ -55,138 +26,71 @@ const ContactForm = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`Title: ${title}\nEmail: ${email}\nMessage: ${message}`);
-    // You can add your own logic here to submit the form data
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    setName('');
+    setEmail('');
+    setMessage('');
   };
-
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
-
+    
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-2 text-black">
-    <div className="w-28 bg-blue-300">
-        <h1 className= "whitespace-pre-wrap w-[340px] font-['Sarabun'] text-[25px] leading-[0.9] font-bold text-left uppercase text-black">FRIENDLY COMMUNITY</h1>             
-    </div>      
-          <ul className='hidden md:flex'>
-              <Link href='/display'>
-                  <li className= "whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Community Display</li>
-              </Link>
-              <Link href='/chat'>
-                  <li className= "whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Channel</li>
-              </Link>
-              <Link href='/contact'>
-                  <li className= "whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Contact Us</li>
-              </Link>
-              <Link href='/login'>
-                  <li className="whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Login</li>
-              </Link>
-              <Link href='/signup'>
-                  <div className="bg-[#ec7777] h-30">
-                      <li className= "whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Sign Up</li>
-                  </div>
-              </Link>     
-          </ul>
-
-     <div style={{ 
-        fontFamily: 'Sarabun',
-        fontSize: '24px',
-        fontWeight: 700,
-        lineHeight: '200px',
-        letterSpacing: '0em',
-        textAlign: 'right',
-        position: 'fixed',
-        top: '5px',
-        left: '400px'
-        }}>
-      <h1>START POSTING COMMUNITY</h1>
-      </div>  
-
-      <div style={{ 
-        fontWeight:30,
-        position: 'fixed',
-        top: '150px',
-        left: '400px'
-        }}>
-  <div style={{width: '45%'}}>
-    <label>
-      TITLE<br/>
-      <input type='text' value={title} onChange={handleTitleChange} required style={{backgroundColor: 'lightgrey'}}/>
-    </label>
-    <br />
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-  <label style={{ marginRight: '20px' }}>
-    LOGO IMAGE<br/>
-    <input type='logoimage' value={logoimage} onChange={handleLogoimagelChange} required style={{ backgroundColor: 'lightgray' }}/>
-  </label>
-  <div style={{ borderLeft: '1px solid black', height: '30px', margin: '0 20px' }}></div>
-  <button style={{color: 'blue'}} onClick={() => document.querySelector('input[type=file]').click()}>CHOOSE FILE</button>
-</div>
-    <br />
-    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-    <label style={{ marginRight: '20px' }}>
-  CITY<br/>
-  <input type='text' value={city} onChange={handleCityChange} required style={{backgroundColor: 'lightgray'}}/>
-</label>
-<label>
-  POSTAL CODE<br/>
-  <input type='text' value={postalCode} onChange={handlePostalCodeChange} required style={{backgroundColor: 'lightgray'}}/>
-</label>
-</div>
-    <br />
-    <label>
-      INSTAGRAM LINK<br/>
-      <input type='instagramlink' value={instagramlink} onChange={handleInstagramlinkChange} required style={{backgroundColor: 'lightgray'}}/>
-    </label>
-    <br />
-    <label>
-      YOUTUBE LINK<br/>
-      <input type='youtubelink' value={youtubelink} onChange={handleYoutubelinkChange} required style={{backgroundColor: 'lightgray'}}/>
-    </label>
-    <br />
-    <label>
-      OTHER WEBSITE LINK<br/>
-      <input type='otherwebsitelink' value={otherwebsitelink} onChange={handleOtherwebsitelinkChange} required style={{backgroundColor: 'lightgray'}}/>
-    </label>
-    <br />
-    <label>
-      EMAIL<br/>
-      <input type='email' value={email} onChange={handleEmailChange} required style={{backgroundColor: 'lightgray'}}/>
-    </label>
-    <br />
-    <label>
-      PHONE NUMBER<br/>
-      <input type='phonenumber' value={phonenumber} onChange={handlePhonenumberChange} required style={{backgroundColor: 'lightgray'}}/>
-    </label>
-    <br />
-    <label>
-      DESCRIPTION<br/>
-      <textarea rows="6" cols="40" type='description' value={description} onChange={handleDescriptionChange} required style={{backgroundColor: 'lightgray'}}/>
-    </label>
-  </div>
-  <div style={{ 
-        fontWeight:30,
-        position: 'fixed',
-        top: '150px',
-        right: '400px'
-        }}>
-    <h2>CONTENT</h2>
-    <textarea rows="20" cols="80" type='message' value={message} onChange={handleMessageChange} required style={{backgroundColor: 'lightgray'}}/>
-    <br />
-    <button type="submit" style={{
-      position: 'absolute',
-      width: '205px',
-      height: '38px',
-      left: '350px',
-      top: '550px',
-      background: '#EC7777'
-      }}>
-  POST NOW
-</button>
+      <div className="w-28 bg-blue-300">
+        <h1 className="whitespace-pre-wrap w-[340px] font-['Sarabun'] text-[25px] leading-[0.9] font-bold text-left uppercase text-black">FRIENDLY COMMUNITY</h1>
+      </div>
+      <ul className="hidden md:flex">
+        <Link href="/display">
+          <li className="whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Community Display</li>
+        </Link>
+        <Link href="/chat">
+          <li className="whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Channel</li>
+        </Link>
+        <Link href="/contact">
+          <li className="whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Contact Us</li>
+        </Link>
+        <Link href="/login">
+          <li className="whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Login</li>
+        </Link>
+        <Link href="/signup">
+          <div className="bg-[#ec7777] h-30">
+            <li className="whitespace-pre-wrap p-4 font-['Sarabun'] text-[20px] leading-[0.9] font-bold text-left text-black">Sign Up</li>
+          </div>
+        </Link>
+      </ul>
+  
+      {/* Add the Contact form here */}
+      <form 
+        className="whitespace-pre-wrap absolute top-[170px] left-[470px] w-[637px] h-[446px] border border-blue-400 shadow-md"
+        style={{ boxShadow: "10px 10px 0 0 #00AFFA" }}
+        onSubmit={handleSubmit}
+      >
+        <h1 style={{fontWeight: "bold"}}>Love to hear from you,</h1>
+        <h2 style={{fontWeight: "bold"}}>Get in touch with us!</h2>
+<br/>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+  <div style={{ marginRight: '10px' }}>
+    <label htmlFor="name">Your Name:</label><br />
+    <input type="text" id="name" value={name} onChange={handleNameChange} required  style={{backgroundColor: '#D9D9D9' , boxShadow: "0px 1px 0px #00AFFA"}}/>
+  </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <div>
+    <label htmlFor="email">Your Email:</label><br />
+    <input type="email" id="email" value={email} onChange={handleEmailChange} required style={{backgroundColor: '#D9D9D9' , boxShadow: "0px 1px 0px #00AFFA"}}/>
   </div>
 </div>
+        <br /><br/>
+        <label>
+  Your Message: <br/>
+  <textarea value={message} onChange={handleMessageChange} required style={{border: '1px solid black', height: '200px', width: '400px'}} />
+</label>
 
+        <br />
+        <button type="submit" style={{background:'black', color:'white', padding: '10px 20px'}}>Submit</button>
+
+
+      </form>
+  
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
 
       <div onClick={handleNav} className='block md:hidden'>
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
@@ -215,6 +119,4 @@ const ContactForm = () => {
      </div>
   );
 };
-
-
-export default ContactForm;
+export default MyComponent;
